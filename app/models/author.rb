ed0f6c4de5a12b4ae863f97facc46b7302927ea2
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-04-15 13:58:02 +1000

    initial commit

---
+++ app/models/author.rb
@@ -0,0 +1,5 @@
+class Author < ApplicationRecord
+    has_one :address, dependent: :destroy
+    # has_one :address, class_name: "Address", foreign_key: "author_id"
+    has_many :books, dependent: :destroy
+end
