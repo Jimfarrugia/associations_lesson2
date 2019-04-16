4060a556a3d9c5ed1a6037c620a9035da5ea85bc
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-04-16 22:50:24 +1000

    books#edit view form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'books/', to: "books#index", as: "books"
   get 'books/new'
   get 'books/:id', to: "books#view", as: "book"
+  put 'books/:id', to: "books#update"
+  patch 'books/:id', to: "books#update"
   get 'books/:id/edit', to: "books#edit", as: "book_edit"
   get 'books/:id/delete', to: "books#delete", as: "book_delete"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
