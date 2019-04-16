fb328f63086559c508a43ef924a07bb065c927bd
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-04-16 14:41:59 +1000

    generated controller with CRUD methods and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/new'
+  get 'books/view'
+  get 'books/edit'
+  get 'books/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/genres", to: "genres#index", as: "genres"
 end
