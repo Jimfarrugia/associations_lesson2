d905d2f1f7d482b47a9f72be20d9b5dd6caaf46c
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-04-16 11:41:36 +1000

    second break day 9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_030038) do
+ActiveRecord::Schema.define(version: 2019_04_16_012409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,23 @@ ActiveRecord::Schema.define(version: 2019_04_15_030038) do
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  create_table "books_genres", force: :cascade do |t|
+    t.bigint "book_id"
+    t.bigint "genre_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_books_genres_on_book_id"
+    t.index ["genre_id"], name: "index_books_genres_on_genre_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "addresses", "authors"
   add_foreign_key "books", "authors"
+  add_foreign_key "books_genres", "books"
+  add_foreign_key "books_genres", "genres"
 end
