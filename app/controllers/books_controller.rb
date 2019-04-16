cdbe6f584f394ba08ba58c05077a87dfa42918e8
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-04-17 00:04:18 +1000

    created books#update method, fixed path in books#edit view form action attribute and fixed :title text_field_tag.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,18 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(title: params[:title])
+    # ISSUE: The line below edits the authors table directly, affecting other (associated)books.
+    # Possible solution:
+    # Create a new Author record if the input isn't found,
+    # else: associate this book with the existing author.
+    book.author.update(name: params[:author])
+
+    redirect_to book_path
+  end
+
   def delete
   end
 end
