1b015ac987227fc3d7842297f9d467f94c818058
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-04-16 23:29:12 +1000

    edited default form values for books@edit and added comments to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
+  # books index
   get 'books/', to: "books#index", as: "books"
+
+  # create book
   get 'books/new'
+
+  # view book
   get 'books/:id', to: "books#view", as: "book"
+
+  # update book
   put 'books/:id', to: "books#update"
   patch 'books/:id', to: "books#update"
   get 'books/:id/edit', to: "books#edit", as: "book_edit"
+
+  # delete book
   get 'books/:id/delete', to: "books#delete", as: "book_delete"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # genres index
   get "/genres", to: "genres#index", as: "genres"
 end
